4b17000213cd631099d3f91ef9010a9c9a585fbf
Vaibh-11 <161677222+Vaibh-11@users.noreply.github.com>
2026-03-30 16:48:30 +0530

    Refactor database connection to use connection pool

--- backend/config/databaseConnection.js
+++ backend/config/databaseConnection.js
@@ -1,14 +1,13 @@
 const mysql = require("mysql2/promise");
 require("dotenv").config();
-
-const connection = mysql.createPool({
+const pool = mysql.createPool({
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  waitForConnections: true,
+  connectionLimit: 10,
 });
 
-console.log("MySQL Connected");
-
-module.exports = connection;
+module.exports = pool;
