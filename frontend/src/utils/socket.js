15461434a28b2051afd62bfbcfab860e71f165b1
Vaibh-11 <161677222+Vaibh-11@users.noreply.github.com>
2026-03-30 16:04:00 +0530

    Delete socket connection code in socket.js
    Remove socket connection setup from socket.js

--- frontend/src/utils/socket.js
+++ frontend/src/utils/socket.js
@@ -1,11 +1,1 @@
-// utils/socket.js
 
-import { io } from "socket.io-client";
-
-
-const socket = io("https://notes-app-production-e699.up.railway.app", {
-  path: "/socket.io",
-  withCredentials: true,
-  transports: ["polling"], 
-});
-export default socket;
