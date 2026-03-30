2506f3fa3c999e02598c5137def828c39ac8bba0
Vaibh-11 <161677222+Vaibh-11@users.noreply.github.com>
2026-03-30 15:06:02 +0530

    Remove websocket transport from socket connection

--- frontend/src/utils/socket.js
+++ frontend/src/utils/socket.js
@@ -5,7 +5,7 @@ import { io } from "socket.io-client";
 
 const socket = io("https://notes-app-production-e699.up.railway.app", {
   withCredentials: true,
-  transports: ["websocket"],
+ 
 });
 
 export default socket;
