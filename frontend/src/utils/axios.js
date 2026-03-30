e9e362b2440d298e11a517f41973ebaf319df098
Vaibh-11 <161677222+Vaibh-11@users.noreply.github.com>
2026-03-30 13:16:55 +0530

    BaseApi change

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://notes-app-production-e699.up.railway.app",
   withCredentials: true,
 });
 
